5547a346b76d161b5c604fa762d1ef8346d9b689
juanluisga <juanluis.garciarodriguez@gmail.com>
2016-12-27 15:27:58 +0100

    cpp

---
+++ cpp/cpp.mm
@@ -0,0 +1,253 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1482838639141"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/cpp.jpeg" heigth="150" width="150"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="codigo" COLOR="#ffff00" BACKGROUND_COLOR="#336600">
+<icon BUILTIN="FreeplaneIcons32px/12Computer/Hardware/laptop"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node POSITION="right" ID="ID_1993014288" CREATED="1482838254537" MODIFIED="1482838639140" HGAP="60" VSHIFT="-60"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/c.png" width="100" heigth="100"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<edge COLOR="#ff0000"/>
+</node>
+<node POSITION="right" ID="ID_1954590083" CREATED="1482838586137" MODIFIED="1482847480850" HGAP="60" VSHIFT="-60"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/cpp_file.jpeg" width="100" heigth="100"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<edge COLOR="#0000ff"/>
+<node ID="ID_530548278" CREATED="1482847440654" MODIFIED="1482848503570" HGAP="50" VSHIFT="-70"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/data_types.png" width="50" heigth="50"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1860024930" CREATED="1482847548609" MODIFIED="1482847912083"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/integer_number.png" width="32" heigth="32"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="SIZE" ID="ID_1591945633" CREATED="1482847567096" MODIFIED="1482847595124">
+<node TEXT="short" STYLE_REF="codigo" ID="ID_1890162846" CREATED="1482847596032" MODIFIED="1482848760125"/>
+<node TEXT="long" STYLE_REF="codigo" ID="ID_1483453564" CREATED="1482847602238" MODIFIED="1482848760129"/>
+</node>
+<node TEXT="sign" ID="ID_1260285003" CREATED="1482847607670" MODIFIED="1482847611203">
+<node TEXT="signed" STYLE_REF="codigo" ID="ID_1558408925" CREATED="1482847613758" MODIFIED="1482848760130"/>
+<node TEXT="unsigned" STYLE_REF="codigo" ID="ID_382468645" CREATED="1482847622182" MODIFIED="1482848760131"/>
+</node>
+</node>
+<node ID="ID_1676575092" CREATED="1482847648269" MODIFIED="1482847801416"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/decimal_number.png" width="32" heigth="32"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="float" STYLE_REF="codigo" ID="ID_221681689" CREATED="1482847812972" MODIFIED="1482848760133"/>
+<node TEXT="double" STYLE_REF="codigo" ID="ID_9328048" CREATED="1482847816612" MODIFIED="1482848760134"/>
+<node TEXT="long double" STYLE_REF="codigo" ID="ID_1591231901" CREATED="1482847819012" MODIFIED="1482848760135"/>
+</node>
+<node ID="ID_1676315111" CREATED="1482848074536" MODIFIED="1482848387351" HGAP="40" VSHIFT="40"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/strings.png" width="32" heigth="32"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_223501972" CREATED="1482848175155" MODIFIED="1482848260600"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/library.jpeg" heigth="48" width="48"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0">
+      <b>string</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<font BOLD="true"/>
+</node>
+<node TEXT="char[]" STYLE_REF="codigo" ID="ID_1307911825" CREATED="1482848236832" MODIFIED="1482848760136" VSHIFT="20"/>
+</node>
+<node ID="ID_1523775999" CREATED="1482848316740" MODIFIED="1482848384798" HGAP="30" VSHIFT="30"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/boolean.png" heigth="50" width="50"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="bool" STYLE_REF="codigo" ID="ID_1416161895" CREATED="1482848396440" MODIFIED="1482848760138"/>
+</node>
+<node ID="ID_1589726867" CREATED="1482848499563" MODIFIED="1482848563762" HGAP="30" VSHIFT="70"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="./images/arrays.png" heigth="50" width="50"/>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1771801177" CREATED="1482848583119" MODIFIED="1482848657181" COLOR="#ffff00" BACKGROUND_COLOR="#336600">
+<icon BUILTIN="FreeplaneIcons32px/12Computer/Hardware/laptop"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>int b[] = {11, 45, 62, 70, 88};</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
