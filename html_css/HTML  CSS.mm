bd6b1a1663fb57fd2cf49ebd25040bb786e3adde
juanluisga <juanluis.garciarodriguez@gmail.com>
2016-12-28 13:28:11 +0100

    html_css ldap mapas mentales subversion

---
+++ html_css/HTML  CSS.mm
@@ -0,0 +1,392 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1440143982033"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      HTML
+    </p>
+    <p style="text-align: center">
+      &amp;
+    </p>
+    <p style="text-align: center">
+      CSS
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<font SIZE="24" BOLD="true"/>
+<node TEXT="HTML" POSITION="right" ID="ID_1447940715" CREATED="1440142316556" MODIFIED="1440142889662" VSHIFT="-200">
+<edge COLOR="#ff0000"/>
+<node TEXT="estructura" ID="ID_1952079428" CREATED="1440142468818" MODIFIED="1440142520454" HGAP="40" VSHIFT="-40">
+<node TEXT="&lt;!DOCTYPE html&gt;&#xa;&lt;html&gt;&#xa;&#xa;&lt;head&gt;&#xa;&lt;title&gt;&#xa;&#xa;&lt;/title&gt;&#xa;&#xa;&lt;/head&gt;&#xa;    &lt;body&gt;&#xa;        &lt;p&gt;&#xa;            P&#xe1;rrafo 1.&#xa;            Blablabla&#xa;        &lt;/p&gt;&#xa;        &lt;p&gt;&#xa;            P&#xe1;rrafo2.&#xa;            Blobloblo&#xa;        &lt;/p&gt;&#xa;    &lt;/body&gt;&#xa;&lt;/html&gt;" ID="ID_370944391" CREATED="1440142454603" MODIFIED="1440144957886" HGAP="70" VSHIFT="-164" STYLE="bubble"/>
+</node>
+<node TEXT="comentarios" ID="ID_958982060" CREATED="1440142884448" MODIFIED="1440142893516" HGAP="70">
+<node TEXT="&lt;!-- un comentario --&gt;" ID="ID_9823416" CREATED="1440142894143" MODIFIED="1440142907874"/>
+</node>
+<node TEXT="head" ID="ID_434452383" CREATED="1440142767896" MODIFIED="1440142784511" HGAP="60" VSHIFT="40">
+<node TEXT="title" ID="ID_1390093333" CREATED="1440142779480" MODIFIED="1440142784510" VSHIFT="-50"/>
+</node>
+<node TEXT="body" ID="ID_1530832743" CREATED="1440142509362" MODIFIED="1440143869166" HGAP="40" VSHIFT="140">
+<node TEXT="Encabezados" ID="ID_1017607996" CREATED="1440142531514" MODIFIED="1440142536535">
+<node TEXT="&lt;h&gt;" ID="ID_1216537722" CREATED="1440142537706" MODIFIED="1440143405956">
+<font BOLD="true"/>
+<node TEXT="1..6" ID="ID_1056551502" CREATED="1440142540181" MODIFIED="1440142543743"/>
+</node>
+</node>
+<node TEXT="P&#xe1;rrafos" ID="ID_1954555895" CREATED="1440142545730" MODIFIED="1440143405297">
+<node TEXT="&lt;p&gt;" ID="ID_1160271316" CREATED="1440142551317" MODIFIED="1440143411476">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="enlaces" ID="ID_1330841807" CREATED="1440142589698" MODIFIED="1440142593986">
+<node TEXT="&lt;a&gt;" ID="ID_693016616" CREATED="1440142593988" MODIFIED="1440143419155">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="imagenes" ID="ID_1567217812" CREATED="1440142597737" MODIFIED="1440142618353">
+<node TEXT="&lt;img&gt;" ID="ID_600273060" CREATED="1440142618358" MODIFIED="1440143427371">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="listas" ID="ID_569849300" CREATED="1440142806040" MODIFIED="1440142843695">
+<node TEXT="ordenadas" ID="ID_1084497906" CREATED="1440142809464" MODIFIED="1440142815173">
+<node TEXT="&lt;ol&gt;" ID="ID_369944971" CREATED="1440142816056" MODIFIED="1440143433620">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="sin orden" ID="ID_1396977525" CREATED="1440142818984" MODIFIED="1440142825788">
+<node TEXT="&lt;ul&gt;" ID="ID_1898896591" CREATED="1440142827280" MODIFIED="1440143438907">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="elemento" ID="ID_249213730" CREATED="1440142835784" MODIFIED="1440142843694" HGAP="40" VSHIFT="40">
+<node TEXT="&lt;li&gt;" ID="ID_1238005753" CREATED="1440142844896" MODIFIED="1440143445578">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="fuente" ID="ID_32896762" CREATED="1440142962207" MODIFIED="1440142978548" VSHIFT="40">
+<node TEXT="style" ID="ID_1098848219" CREATED="1440143093350" MODIFIED="1440143095539">
+<node TEXT="tama&#xf1;o" ID="ID_282315705" CREATED="1440142970535" MODIFIED="1440142974196">
+<node TEXT="&lt;p style = &quot;font-size: 12px&quot;&gt;" ID="ID_1143235804" CREATED="1440142992151" MODIFIED="1440143346750">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="color" ID="ID_1546812461" CREATED="1440143018799" MODIFIED="1440143020723">
+<node TEXT="&lt;h2 style=&quot;color:red&quot;&gt;" ID="ID_73474055" CREATED="1440143021654" MODIFIED="1440143346753">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="familia" ID="ID_1216615617" CREATED="1440143043118" MODIFIED="1440143045955">
+<node TEXT="&lt;h1 style=&quot;font-family: Arial&quot;&gt;Title&lt;/h1&gt;" ID="ID_496135996" CREATED="1440143046446" MODIFIED="1440143346753">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="fondo" ID="ID_579320335" CREATED="1440143105398" MODIFIED="1440143108507">
+<node TEXT="&lt;p style=&quot;background-color: red;&quot;&gt;&#xa1;Hola!&lt;/p&gt;" ID="ID_943390289" CREATED="1440143119414" MODIFIED="1440143346754">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="alineaci&#xf3;n" ID="ID_1585371423" CREATED="1440143146438" MODIFIED="1440143149594">
+<node TEXT="&lt;h1 style=&quot;text-align:center&quot;&gt;" ID="ID_1652646930" CREATED="1440143288621" MODIFIED="1440143346755">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="negrita" ID="ID_1973041183" CREATED="1440143306525" MODIFIED="1440143328131">
+<node TEXT="&lt;strong&gt;" ID="ID_1837502656" CREATED="1440143311325" MODIFIED="1440143511107">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="it&#xe1;lica" ID="ID_1723799183" CREATED="1440143373132" MODIFIED="1440143378521">
+<node TEXT="&lt;em&gt;" ID="ID_1293316424" CREATED="1440143393212" MODIFIED="1440143397523">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="tablas" ID="ID_390813869" CREATED="1440143512875" MODIFIED="1440143609160" HGAP="30" VSHIFT="60">
+<node TEXT="Encabezado" ID="ID_909059593" CREATED="1440143612259" MODIFIED="1440143619791">
+<node TEXT="&lt;thead&gt;" ID="ID_394649216" CREATED="1440143653682" MODIFIED="1440143711550">
+<font BOLD="true"/>
+</node>
+<node TEXT="t&#xed;tulo de columna" ID="ID_933040023" CREATED="1440143662602" MODIFIED="1440143685319">
+<node TEXT="&lt;th&gt;" ID="ID_1668194696" CREATED="1440143686386" MODIFIED="1440143711552">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="Cuerpo" ID="ID_1395889243" CREATED="1440143620234" MODIFIED="1440143624231">
+<node TEXT="&lt;tbody&gt;" ID="ID_798871049" CREATED="1440143624746" MODIFIED="1440143711552">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="filas" ID="ID_476082096" CREATED="1440143515987" MODIFIED="1440143536263">
+<node TEXT="&lt;tr&gt;" ID="ID_977772480" CREATED="1440143537539" MODIFIED="1440143711553">
+<font BOLD="true"/>
+</node>
+<node TEXT="extender" ID="ID_852902145" CREATED="1440143769202" MODIFIED="1440143774610">
+<node TEXT="rowspan" ID="ID_901645205" CREATED="1440143774612" MODIFIED="1440143817561">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="columnas" ID="ID_1563229036" CREATED="1440143553779" MODIFIED="1440143567215">
+<node TEXT="&lt;td&gt;" ID="ID_648236723" CREATED="1440143568123" MODIFIED="1440143711553">
+<font BOLD="true"/>
+</node>
+<node TEXT="extender" ID="ID_1019780144" CREATED="1440143750986" MODIFIED="1440143755170">
+<node TEXT="colspan" ID="ID_1973570598" CREATED="1440143755172" MODIFIED="1440143764446">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="estilos" ID="ID_758715386" CREATED="1440143819201" MODIFIED="1440143822921">
+<node TEXT="style" ID="ID_777383441" CREATED="1440143822923" MODIFIED="1440143827566">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="contenedores" ID="ID_1606932277" CREATED="1440143857057" MODIFIED="1440143876518" VSHIFT="60">
+<node TEXT="&lt;div&gt;" ID="ID_1067226702" CREATED="1440143879729" MODIFIED="1440143886366">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="partes de texto" ID="ID_1449149445" CREATED="1440143921136" MODIFIED="1440143926221">
+<node TEXT="&lt;span&gt;" ID="ID_1980305322" CREATED="1440143927896" MODIFIED="1440143934727">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="CSS" POSITION="right" ID="ID_111374245" CREATED="1440142319525" MODIFIED="1440145371484" HGAP="60" VSHIFT="-70">
+<edge COLOR="#0000ff"/>
+<node TEXT="enlace con html (head)" ID="ID_420836654" CREATED="1440143977368" MODIFIED="1440144043290" HGAP="60" VSHIFT="-150">
+<node TEXT="&lt;link type=&quot;text/css&quot; rel=&quot;stylesheet&quot; href=&quot;stylesheet.css&quot;/&gt;" ID="ID_817099978" CREATED="1440144018712" MODIFIED="1440144043289" VSHIFT="-60">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="sintaxis" ID="ID_85301475" CREATED="1440144112655" MODIFIED="1440144608954">
+<node TEXT="selectores" ID="ID_319339162" CREATED="1440144490989" MODIFIED="1440144493985">
+<node TEXT="selector {&#xa;    propiedad: valor;&#xa;}" ID="ID_770434672" CREATED="1440144123799" MODIFIED="1440144448309" STYLE="bubble">
+<font BOLD="true"/>
+</node>
+<node TEXT="anidamientos" ID="ID_228990695" CREATED="1440144421165" MODIFIED="1440144426850">
+<node TEXT="div div p {&#xa;    /*&#xa1;Lo de CSS!*/&#xa;}" ID="ID_1943507480" CREATED="1440144430693" MODIFIED="1440144448311" STYLE="bubble">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="universal" ID="ID_1555301151" CREATED="1440144503589" MODIFIED="1440144507385">
+<node TEXT="*" ID="ID_1559190149" CREATED="1440144508308" MODIFIED="1440144590296">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="hijos directos" ID="ID_1332784351" CREATED="1440144592028" MODIFIED="1440144596220">
+<node TEXT="div &gt; p { /* C&#xf3;digo de CSS */ }" ID="ID_1101305307" CREATED="1440144596222" MODIFIED="1440144600585">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="clases" ID="ID_1270461310" CREATED="1440144651155" MODIFIED="1440144654136">
+<node TEXT="." ID="ID_373869814" CREATED="1440144655331" MODIFIED="1440144679984">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="identificadores" ID="ID_1776874020" CREATED="1440144682467" MODIFIED="1440144686808">
+<node TEXT="#" ID="ID_68887805" CREATED="1440144687691" MODIFIED="1440144732223">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="pseudoclases" ID="ID_203887375" CREATED="1440144733939" MODIFIED="1440144752693">
+<node TEXT="selector:selector_de_pseudoclase {&#xa;    propiedad: valor;&#xa;}" ID="ID_629619990" CREATED="1440144739523" MODIFIED="1440144752690" STYLE="bubble" HGAP="40" VSHIFT="30">
+<font BOLD="true"/>
+</node>
+<node TEXT="&lt;a&gt;" ID="ID_733689326" CREATED="1440144773067" MODIFIED="1440144789403">
+<font BOLD="true"/>
+<node TEXT="link" ID="ID_859606271" CREATED="1440144789404" MODIFIED="1440144812769">
+<font BOLD="true"/>
+</node>
+<node TEXT="visited" ID="ID_821858997" CREATED="1440144804538" MODIFIED="1440144812769">
+<font BOLD="true"/>
+</node>
+<node TEXT="hover" ID="ID_1408019155" CREATED="1440144807818" MODIFIED="1440144812767">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="primer hijo" ID="ID_1118325396" CREATED="1440144831778" MODIFIED="1440144836906">
+<node TEXT="first-child" ID="ID_498359477" CREATED="1440144836908" MODIFIED="1440144844943">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="N-hijo" ID="ID_147451034" CREATED="1440144853786" MODIFIED="1440144856358">
+<node TEXT="nth-child(n)" ID="ID_568683031" CREATED="1440144869130" MODIFIED="1440144876151">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="comentario" ID="ID_997850583" CREATED="1440144189663" MODIFIED="1440144608953" HGAP="50" VSHIFT="40">
+<node TEXT="/*&#xa1;Un comentario!*/" ID="ID_204802626" CREATED="1440144194217" MODIFIED="1440144208403">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="tama&#xf1;os" ID="ID_704892443" CREATED="1440144254838" MODIFIED="1440144604257" HGAP="30" VSHIFT="100">
+<node TEXT="pixeles" ID="ID_827891795" CREATED="1440144258734" MODIFIED="1440144263619">
+<node TEXT="px" ID="ID_596821668" CREATED="1440144264574" MODIFIED="1440144268691">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="relativa" ID="ID_1364724043" CREATED="1440144269734" MODIFIED="1440144284314">
+<node TEXT="em" ID="ID_151363820" CREATED="1440144284678" MODIFIED="1440144291651">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="posicionamiento" ID="ID_1423571369" CREATED="1440144914114" MODIFIED="1440145371483" VSHIFT="30">
+<node TEXT="display" ID="ID_620299586" CREATED="1440145288383" MODIFIED="1440145292500">
+<font BOLD="true"/>
+<node TEXT="block" ID="ID_1720779276" CREATED="1440145317775" MODIFIED="1440145367997">
+<font BOLD="true"/>
+</node>
+<node TEXT="inline-block" ID="ID_403969165" CREATED="1440145332663" MODIFIED="1440145367997">
+<font BOLD="true"/>
+</node>
+<node TEXT="Inline" ID="ID_397838291" CREATED="1440145336871" MODIFIED="1440145367998">
+<font BOLD="true"/>
+</node>
+<node TEXT="none" ID="ID_1735756382" CREATED="1440145346967" MODIFIED="1440145367995">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="margin" ID="ID_66858022" CREATED="1440145413078" MODIFIED="1440145421659">
+<font BOLD="true"/>
+<node TEXT="top-margin" ID="ID_1421938307" CREATED="1440145422798" MODIFIED="1440145430555">
+<font BOLD="true"/>
+</node>
+<node TEXT="right-margin" ID="ID_1509120745" CREATED="1440145432606" MODIFIED="1440145439395">
+<font BOLD="true"/>
+</node>
+<node TEXT="bottom-margin" ID="ID_1658621275" CREATED="1440145440238" MODIFIED="1440145448859">
+<font BOLD="true"/>
+</node>
+<node TEXT="left-margin" ID="ID_646144899" CREATED="1440145449982" MODIFIED="1440145487361">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="borde" ID="ID_1466461259" CREATED="1440145488670" MODIFIED="1440145490314">
+<node TEXT="border" ID="ID_485680627" CREATED="1440145491238" MODIFIED="1440145500459">
+<font BOLD="true"/>
+</node>
+<node TEXT="border-radius" ID="ID_1433545435" CREATED="1440145493382" MODIFIED="1440145500458">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="relleno" ID="ID_1271438152" CREATED="1440145556741" MODIFIED="1440145558474">
+<node TEXT="padding" ID="ID_330217126" CREATED="1440145560373" MODIFIED="1440145592570">
+<font BOLD="true"/>
+<node TEXT="top-padding" ID="ID_952183679" CREATED="1440145567253" MODIFIED="1440145592571">
+<font BOLD="true"/>
+</node>
+<node TEXT="right-padding" ID="ID_245912889" CREATED="1440145572261" MODIFIED="1440145592571">
+<font BOLD="true"/>
+</node>
+<node TEXT="bottom-padding" ID="ID_1637798227" CREATED="1440145578125" MODIFIED="1440145592571">
+<font BOLD="true"/>
+</node>
+<node TEXT="left-padding" ID="ID_1347849484" CREATED="1440145582909" MODIFIED="1440145592572">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="flotaci&#xf3;n" ID="ID_1264619973" CREATED="1440145629277" MODIFIED="1440145634569">
+<node TEXT="float" ID="ID_791013319" CREATED="1440145635285" MODIFIED="1440145639185">
+<font BOLD="true"/>
+<node TEXT="left" ID="ID_1185996571" CREATED="1440145653949" MODIFIED="1440145663338">
+<font BOLD="true"/>
+</node>
+<node TEXT="right" ID="ID_1277421466" CREATED="1440145656317" MODIFIED="1440145664273">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="despegar" ID="ID_1006262907" CREATED="1440145700028" MODIFIED="1440145703124">
+<node TEXT="clear" ID="ID_993658562" CREATED="1440145703126" MODIFIED="1440145706225">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="posicionamiento" ID="ID_1455521590" CREATED="1440145775452" MODIFIED="1440145780204">
+<node TEXT="position" ID="ID_1848232438" CREATED="1440145780206" MODIFIED="1440145787356">
+<font BOLD="true"/>
+<node TEXT="static" ID="ID_634561625" CREATED="1440145787358" MODIFIED="1440145792408">
+<font BOLD="true"/>
+</node>
+<node TEXT="absolute" ID="ID_1260814220" CREATED="1440145793316" MODIFIED="1440145826608">
+<font BOLD="true"/>
+</node>
+<node TEXT="relative" ID="ID_802573257" CREATED="1440145827092" MODIFIED="1440145865248">
+<font BOLD="true"/>
+</node>
+<node TEXT="fixed" ID="ID_1326058308" CREATED="1440145865587" MODIFIED="1440145868856">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ mapas mentales/CARTOGRAFÍA MENTAL.mm
@@ -0,0 +1,168 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#80d8e8" CREATED="1293447519389" ID="ID_1848003173" MODIFIED="1293447570845" TEXT="CARTOGRAF&#xcd;A MENTAL">
+<font BOLD="true" NAME="SansSerif" SIZE="28"/>
+<node CREATED="1293447576225" ID="ID_1300993219" MODIFIED="1293454653012" POSITION="right" TEXT="T&#xc9;CNICAS">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node CREATED="1293447599232" ID="ID_1230298203" MODIFIED="1293454663099" TEXT="&#xe9;nfasis">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1293447769477" ID="ID_399695171" MODIFIED="1293454680934" TEXT="imagen central">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1293447781645" ID="ID_596037576" MODIFIED="1293454680934" TEXT="im&#xe1;genes en todo el mapa">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1293447797908" ID="ID_773921426" MODIFIED="1293454680933" TEXT="tres o m&#xe1;s colores en cada imagen central">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1293447822236" ID="ID_366780698" MODIFIED="1293454680933" TEXT="dimensi&#xf3;n en im&#xe1;genes y palabras">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1293447838428" ID="ID_1375914854" MODIFIED="1293454680933" TEXT="sinestesia">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1293447842820" ID="ID_1215337569" MODIFIED="1293447851247" TEXT="fusi&#xf3;n de sentidos f&#xed;sicos"/>
+</node>
+<node CREATED="1293447857651" ID="ID_1970681965" MODIFIED="1293454680933" TEXT="variaci&#xf3;n de tama&#xf1;os">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1293447866387" ID="ID_1724959019" MODIFIED="1293447869303" TEXT="letras"/>
+<node CREATED="1293447869995" ID="ID_985385332" MODIFIED="1293447873359" TEXT="l&#xed;neas"/>
+<node CREATED="1293447873851" ID="ID_1310897645" MODIFIED="1293447876575" TEXT="im&#xe1;genes"/>
+</node>
+<node CREATED="1293447881587" ID="ID_696040607" MODIFIED="1293454680932" TEXT="organizaci&#xf3;n del espacio">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1293447611448" ID="ID_583186074" MODIFIED="1293454663101" TEXT="asociaci&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1293447897491" ID="ID_868236280" MODIFIED="1293454680932" TEXT="conexiones dentro y entre ramas">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1293447929938" ID="ID_1674742095" MODIFIED="1293447932230" TEXT="flechas">
+<node CREATED="1293449490024" ID="ID_1131262078" MODIFIED="1293449498612" TEXT="multidireccionales"/>
+<node CREATED="1293449501520" ID="ID_579290575" MODIFIED="1293449513364" TEXT="tama&#xf1;o"/>
+<node CREATED="1293449513768" ID="ID_1852863116" MODIFIED="1293449515212" TEXT="forma"/>
+<node CREATED="1293449519680" ID="ID_1659814679" MODIFIED="1293449522788" TEXT="dimensi&#xf3;n"/>
+</node>
+</node>
+<node CREATED="1293447939250" ID="ID_47056838" MODIFIED="1293454680932" TEXT="colores">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1293447944178" ID="ID_1632815426" MODIFIED="1293454680931" TEXT="c&#xf3;digos">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1293447618695" ID="ID_1174110172" MODIFIED="1293454663100" TEXT="claridad">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1293447952514" ID="ID_860423374" MODIFIED="1293454680931" TEXT="no m&#xe1;s de una palabra por l&#xed;nea">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1293447964002" ID="ID_1581742546" MODIFIED="1293454680930" TEXT="letra de imprenta">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node CREATED="1293447976601" ID="ID_445489654" MODIFIED="1293454680929" TEXT="palabras clave sobre l&#xed;neas">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1293448090048" ID="ID_560849174" MODIFIED="1293448100267" TEXT="l&#xed;neas y palabras de igual longitud"/>
+<node CREATED="1293448106943" ID="ID_1376213363" MODIFIED="1293448121147" TEXT="l&#xed;neas unidas entre s&#xed;"/>
+<node CREATED="1293448122375" ID="ID_358533140" MODIFIED="1293454727652" TEXT="ramas mayores unidas con imagen central">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1293448153783" ID="ID_1988038509" MODIFIED="1293448183938" TEXT="l&#xed;neas centrales">
+<node CREATED="1293448174702" ID="ID_206231900" MODIFIED="1293448178586" TEXT="m&#xe1;s gruesas"/>
+<node CREATED="1293448161998" ID="ID_1767266711" MODIFIED="1293448168714" TEXT="con forma org&#xe1;nica"/>
+</node>
+<node CREATED="1293448197550" ID="ID_1778018713" MODIFIED="1293448238649" TEXT="enlazar l&#xed;mites con rama de palabra clave"/>
+</node>
+<node CREATED="1293448243005" ID="ID_1612818550" MODIFIED="1293454680927" TEXT="im&#xe1;genes claras">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1293447624375" ID="ID_1898871269" MODIFIED="1293454663100" TEXT="estilo personal">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1293447664111" ID="ID_896985874" MODIFIED="1293454699739" POSITION="right" TEXT="DIAGRAMACI&#xd3;N">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1293447673998" ID="ID_1344359902" MODIFIED="1293454707774" TEXT="jerarqu&#xed;a">
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1293447686270" ID="ID_1763375546" MODIFIED="1293454707775" TEXT="orden num&#xe9;rico">
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1293447714742" ID="ID_969487233" MODIFIED="1293454736390" POSITION="right" TEXT="RECOMENDACIONES">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1293447725838" ID="ID_1870542641" MODIFIED="1293454745458" TEXT="rompe bloqueos mentales">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1293450058607" ID="ID_258045221" MODIFIED="1293454765102" TEXT="a&#xf1;adir l&#xed;neas en blanco">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1293450072479" ID="ID_507123170" MODIFIED="1293454765103" TEXT="hacer preguntas">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1293450077991" ID="ID_1275854117" MODIFIED="1293454765103" TEXT="a&#xf1;adir im&#xe1;genes">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1293450085375" ID="ID_332948975" MODIFIED="1293454765103" TEXT="no olvidar que la capacidad asociativa es infinita">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1293447736805" ID="ID_775071041" MODIFIED="1293454745462" TEXT="refuerza">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1293450102543" ID="ID_198379167" MODIFIED="1293454765102" TEXT="revisar mapas mentales">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1293450114646" ID="ID_1914083477" MODIFIED="1293454765102" TEXT="reconocimiento r&#xe1;pido de mapa mental">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1293447740965" ID="ID_1395190911" MODIFIED="1293454745461" TEXT="prepara">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1293450126742" ID="ID_1481878728" MODIFIED="1293454765102" TEXT="actitud mental">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1293450135054" ID="ID_479647210" MODIFIED="1293450138810" TEXT="positiva"/>
+<node CREATED="1293450139398" ID="ID_1386606368" MODIFIED="1293450149025" TEXT="copiar im&#xe1;genes del entorno"/>
+<node CREATED="1293450150838" ID="ID_1034249171" MODIFIED="1293450153313" TEXT="confianza"/>
+<node CREATED="1293450155198" ID="ID_263070215" MODIFIED="1293450166561" TEXT="&#xa1;no rechazar el absurdo!"/>
+<node CREATED="1293450168382" ID="ID_1230210216" MODIFIED="1293450182449" TEXT="construir belleza"/>
+</node>
+<node CREATED="1293450191333" ID="ID_629542385" MODIFIED="1293454765102" TEXT="materiales">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1293450195893" ID="ID_907898113" MODIFIED="1293454765102" TEXT="ambiente de trabajo">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1293450202325" ID="ID_19431602" MODIFIED="1293450206456" TEXT="temperatura"/>
+<node CREATED="1293450209189" ID="ID_1328795947" MODIFIED="1293450223160" TEXT="luz natural"/>
+<node CREATED="1293450225948" ID="ID_1794871039" MODIFIED="1293450231552" TEXT="ventilaci&#xf3;n"/>
+<node CREATED="1293450233180" ID="ID_1312427394" MODIFIED="1293450236768" TEXT="mobiliario"/>
+<node CREATED="1293450238276" ID="ID_680192508" MODIFIED="1293450244600" TEXT="agradable"/>
+<node CREATED="1293450246388" ID="ID_1981347970" MODIFIED="1293450250360" TEXT="sonido">
+<node CREATED="1293450251364" ID="ID_34469251" MODIFIED="1293450256192" TEXT="musica agradable"/>
+<node CREATED="1293450256780" ID="ID_1417325659" MODIFIED="1293450259704" TEXT="silencio"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1293454276418" ID="ID_785278366" MODIFIED="1293454279909" POSITION="right" TEXT="RIESGOS">
+<node CREATED="1293454282210" ID="ID_1944558754" MODIFIED="1293454295965" TEXT="mapas mentales que en realidad no lo son">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1293454297185" ID="ID_722491109" MODIFIED="1293454319581" TEXT="la idea de que las frases son m&#xe1;s significativas">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1293454320809" ID="ID_1577976976" MODIFIED="1293454368620" TEXT="la idea de que un mapa mental &quot;desordenado&quot; no sirve">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1293454349696" ID="ID_1209069306" MODIFIED="1293454372140" TEXT="una reacci&#xf3;n negativa ante un mapa mental cualquiera">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+</node>
+</map>
